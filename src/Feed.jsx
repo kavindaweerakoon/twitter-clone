a986d9d3e5fecb316b8dcfb22f93b5f9f14f1839
Kavinda Weerakoon <kw@kwnetic.com>
2022-04-17 03:15:52 -0400

    firebase init + fetch

--- src/Feed.jsx
+++ src/Feed.jsx
@@ -1,19 +1,57 @@
-import React from "react";
+
+import React, { useEffect, useState } from "react";
 import "./Feed.css";
+import {
+  db,
+  collection,  
+  orderBy,
+  query,
+  onSnapshot,
+} from "./firebase";
 import Post from "./Post";
 import Tweetbox from "./Tweetbox";
 
 function Feed() {
+
+  const [posts, setPosts] = useState([]);
+  
+  useEffect(() => {
+    const getPosts = onSnapshot(
+      query(collection(db, "posts"), orderBy("timestamp", "desc")),
+      (snapshot) => {
+        const posts = snapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            data: doc.data(),
+          };
+        });
+        setPosts(posts);
+        
+      })
+    return () => getPosts();
+  }, []);
+
   return (
     <div className="feed">
       <div className="feed__header">
         <h2>Home</h2>
       </div>
 
-      <Tweetbox/>
+      <Tweetbox />
 
-      <Post/>
-     
+      {posts.map(({id, data: {displayName, username, verified, text, avatar, image, timestamp}}) => (
+
+
+      <Post
+        key={id}
+        displayName={displayName}
+        username={username}
+        verified={verified}
+        text={text}
+        avatar={avatar}
+        image={image}
+        timestamp={timestamp}
+      /> ))}
     </div>
   );
 }
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,36 +1,47 @@
 import React from "react";
 import "./Post.css";
 import { Avatar } from "@mui/material";
-import VerifiedIcon from '@mui/icons-material/Verified';
-import { ChatBubbleOutline, ChatBubbleOutlineOutlined, FavoriteBorder, Publish, Repeat } from "@mui/icons-material";
+import VerifiedIcon from "@mui/icons-material/Verified";
+import {
+  ChatBubbleOutline,
+  ChatBubbleOutlineOutlined,
+  FavoriteBorder,
+  Publish,
+  Repeat,
+} from "@mui/icons-material";
 
-function Post({ displayName, username, verified, text, image, avatar }) {
+function Post({ displayName, username, verified, text, image, avatar, timestamp }) {
+  
+  const dateObject = new Date(timestamp*1000).toLocaleString( "en-US", {
+    month: "short",
+    day: "numeric",
+  })
+
+  
   return (
     <div className="post">
       <div className="post__avatar">
-        <Avatar src="https://pbs.twimg.com/profile_images/1225105869/profile_normal.jpg " />
+        <Avatar src={avatar} />
       </div>
       <div className="post__body">
         <div className="post__header">
           <div className="post__headerText">
             <h3>
-              Kavinda W{" "}
+              {displayName}{" "}
               <span className="post__headerSpecial">
-                <VerifiedIcon className="post__badge" />
-                @kavinda_w
+                {verified && <VerifiedIcon className="post__badge" />} @
+                {username} • {dateObject}
               </span>
             </h3>
           </div>
-          <div className="post__headerDescription">
-            "Hey Jeff can I get a raise? My family is starving."
-          </div>
+          <div className="post__headerDescription">{text}</div>
         </div>
-        <img src="https://media1.giphy.com/media/Qc4i40SWi42rK/giphy.gif?cid=790b7611d1d705e2c8372b65bdb2ebcc0377065701e2eb73&rid=giphy.gif&ct=g" alt=""/>
+        <img src={image} alt="" />
         <div className="post__footer">
-            <ChatBubbleOutline fontSize="small" />
-            <Repeat fontSize="small" />
-            <FavoriteBorder fontSize="small" />
-            <Publish fontSize="small" />
+          <ChatBubbleOutline fontSize="small" />
+          <Repeat fontSize="small" />
+          <FavoriteBorder fontSize="small" />
+          <Publish fontSize="small" />
         </div>
       </div>
     </div>
