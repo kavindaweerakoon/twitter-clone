957b90fd9a6a6aa9561b059a110f6203882bfe3f
Kavinda Weerakoon <kw@kwnetic.com>
2022-04-17 23:57:34 -0400

    cleanup 4/17/2022

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Sidebar from './Sidebar';
 import Widgets from './Widgets';
 
 
+
+
 function App() {
   return (
     <div className="app">
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleLogin } from 'react-google-login';
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
